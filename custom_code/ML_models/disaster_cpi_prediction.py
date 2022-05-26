05d92c4e5f47bc49d2cb3736d786d25d224c7f8a
Yue Wu <43990174+PaulWooZJU@users.noreply.github.com>
2022-05-26 11:33:40 -0700

    Add files via upload

---
+++ custom_code/ML_models/disaster_cpi_prediction.py
@@ -0,0 +1,177 @@
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+import geoviews as gv
+
+import xgboost as xgb
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+
+gv.extension("bokeh")
+
+
+def plot_worldwide_choroplethmap(df, title_name):
+    """
+    Func plot_interactive_sacttermap is designed to plot earthquake locations worldwide scatter map.
+
+    Attributes:
+    df(pd.DataFrame): Dataset with 'country_name' and 'CPI' info
+    title_name(str): Plot title name
+
+    Returns:
+    Worldwide Choropleth Map
+    """
+
+    # WorldMap Background
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    world = world[(world.name!="Antarctica")]
+    
+    preds = world.merge(df, left_on='name', right_on='country_name')
+
+    img = gv.Polygons(data=preds, vdims=["CPI", "name"]).opts(height=400, width=600, title=title_name, colorbar=True, tools=['hover',], hover_color='lime')
+
+    return img
+
+# Worldwide disaster CPI Prediction
+def one_hot_encode(df, name):
+    """
+    Func one_hot_encode is designed to do one-hot encoding.
+    
+    Attributes:
+    df(pd.DataFrame): Dataset we use
+    name(str): One-hot encoding target
+
+    Returns:
+    encoder_df(pd.DataFrame): One-hot encoding results
+    """
+    assert isinstance(df, pd.DataFrame)
+    assert isinstance(name, str)
+
+    encoder = OneHotEncoder(handle_unknown='ignore')
+    encoder_df = pd.DataFrame(encoder.fit_transform(df[[name]]).toarray())
+
+    return encoder_df
+
+def prepare_worldwide_disaster_data(file_name = '1970-2022_DISASTERS.xlsx - emdat data.csv'):
+    """
+    Func prepare_worldwide_disaster_data is designed to make preparation and feature extraction.
+
+    Attributes:
+    file_name(str): file_path
+
+    Returns:
+    df_final(pd.DataFrame): Dataset ready to do ML model
+    features(list): What we extraction from original dataset
+    """
+    #df = pd.read_csv ('./datasets/1970-2022_DISASTERS.xlsx - emdat data.csv')
+    # Pretreatment
+    df = pd.read_csv(file_name)
+    df=df.drop(columns=['Dis No','Seq','Glide','Disaster Group', 'OFDA Response', 'Appeal', 'Declaration', 'Aid Contribution', 
+                        'Associated Dis', 'Associated Dis2', 'Dis Mag Value', 'Dis Mag Scale',
+                        'Insured Damages (\'000 US$)', 'Adm Level', 'Admin1 Code', 'Admin2 Code', 'Event Name'])
+
+    df = df[~(df['Disaster Type'].isin(["Insect infestation", "Animal accident","Impact","Glacial lake outburst","Fog","Mass movement (dry)", 'Epidemic', 'Drought', 'Volcanic activity']))]
+    # Feature Extraction
+    df_sub = df[['Year', 'Disaster Type', 'Country', 'ISO', 'Region', 'Continent', 'Location', 'Start Year', 'Start Month', 'Start Day', 'End Year', 'End Month', 'End Day', 'Total Deaths', 'Total Damages (\'000 US$)', 'CPI']]
+    df_sub = df_sub.rename(columns={'Total Damages (\'000 US$)': 'Total Damages'})
+    # Filter ISO frequency less than 100
+    df_sub_ = df_sub.copy().groupby('ISO').filter(lambda x: len(x)>100)
+    # HKG not shown
+    df_sub_ = df_sub_.loc[df_sub_['ISO'] !=  "HKG"]
+    # disasters and ISO codebook
+    #disaster_codebook = dict(zip([i for i in range(len(df_sub_['Disaster Type'].unique()))], df_sub_['Disaster Type'].unique()))
+    #country_codebook = dict(zip([i for i in range(len(df_sub_['ISO'].unique()))], df_sub_['ISO'].unique()))
+    # Filter Year Interval bigger than 0
+    df_sub_['Year Interval'] = df_sub_['End Year'] - df_sub_['Start Year']
+    df_sub_ = df_sub_[df_sub_['Year Interval']<1]
+    # Get features what we need
+    df_sample = df_sub_[['Year', 'Disaster Type', 'Country', 'ISO', 'Region', 'Continent', 'Location', 'Start Month', 'End Month', 'Total Deaths', 'Total Damages','CPI']]
+    df_sample = df_sample.reset_index(drop=True)
+    # One-Hot encoding
+    # Disaster Type encoding
+    encoder_df1 = one_hot_encode(df_sample, 'Disaster Type')
+    df_encoder1 = df_sample.copy().join(encoder_df1)
+    disaster_cnt = len(df_encoder1['Disaster Type'].unique())
+    assert disaster_cnt == 6
+    disaster_codebook = dict(zip([i for i in range(disaster_cnt)], sorted(df_encoder1['Disaster Type'].unique())))
+    df_encoder1.drop('Disaster Type', axis=1, inplace=True)
+    df_encoder1 = df_encoder1.rename(columns=disaster_codebook)
+    # ISO encoding
+    encoder_df2 = one_hot_encode(df_encoder1, 'ISO')
+    df_encoder2 = df_encoder1.copy().join(encoder_df2)
+    country_cnt = len(df_encoder2['ISO'].unique())
+    assert country_cnt == 26
+    country_codebook = dict(zip([i for i in range(country_cnt)], sorted(df_encoder2['ISO'].unique())))
+    df_encoder2.drop('ISO', axis=1, inplace=True)
+    df_encoder2 = df_encoder2.rename(columns=country_codebook)
+    # Output
+    df_output = df_encoder2.copy().dropna(subset=['CPI'])
+    df_output = df_output.dropna(subset=['Start Month'])
+    df_output = df_output.reset_index(drop=True)
+    df_output['Month Interval'] = df_output['End Month']-df_output['Start Month']
+    # features
+    features = ['Year', 'Start Month', 'Month Interval'] + list(disaster_codebook.values()) + list(country_codebook.values()) + ['CPI']
+    df_final = df_output[features]
+
+    return df_final, features
+
+def predict_cpi_model(df, features, year=2023, month=7, disaster='Wildfire'):
+    """
+    Func predict_cpi_model is designed to find CPI of a specific disaster type  across the world in the future.
+
+    Attributes:
+    df(pd.DataFrame): Dataset after preparation and feature extraction
+    featrues(list): Feature extraction
+    year(int): One in the future
+    month(int): One in the future
+    disaster(str): One type from ['Earthquake', 'Extreme temperature', 'Flood', 'Landslide', 'Storm', 'Wildfire']
+
+    Returns:
+    df_preds(pd.DataFrame): [country_name, CPI]
+    title_name(str): Title of the plot
+    """
+    assert isinstance(df, pd.DataFrame)
+    assert isinstance(features, list)
+    assert isinstance(year, int)
+    assert isinstance(month, int)
+    assert isinstance(disaster, str)
+    assert 1 <= month <= 12
+    assert disaster in ['Earthquake', 'Extreme temperature', 'Flood', 'Landslide', 'Storm', 'Wildfire']
+    
+    X = df.loc[:, df.columns != 'CPI']
+    y = df.loc[:, df.columns == 'CPI']
+    
+    data_dmatrix = xgb.DMatrix(data=X,label=y)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+
+    xg_reg = xgb.XGBRegressor(objective ='reg:linear', colsample_bytree = 0.3, learning_rate = 0.1,
+                              max_depth = 5, alpha = 10, n_estimators = 10)
+
+    xg_reg = xg_reg.fit(X_train,y_train)
+    
+    # ['Year', 'Start Month', 'Month Interval'] + list(disaster_codebook.values()) + list(country_codebook.values()) + ['CPI']
+    X_preds = pd.DataFrame(0, index=[i for i in range(26)], columns=features[:-1])
+    X_preds.loc[:, 'Year'] = year
+    X_preds.loc[:, 'Start Month'] = month
+
+    country_codebook_names = ['AFG', 'ARG', 'AUS', 'BGD', 'BRA', 'CAN', 'CHN', 'COL', 'FRA', 'HTI', 'IDN', 'IND', 'IRN', 'ITA', 'JPN', 'KOR', 'MEX', 'NPL', 'PAK', 'PER', 'PHL', 'RUS', 'THA', 'TUR', 'USA', 'VNM']
+    for i in X_preds.index:
+        X_preds.at[i, country_codebook_names[i]] = 1
+
+    X_preds.loc[:, disaster] = 1
+
+    y_preds = xg_reg.predict(X_preds)
+
+    country_name = np.array(['Afghanistan', 'Argentina', 'Australia', 'Bangladesh', 'Brazil', 'Canada', 'China', 'Colombia', 'France', 'Haiti', 'Indonesia',
+                             'India', 'Iran', 'Italy', 'Japan', 'South Korea', 'Mexico', 'Nepal', 'Pakistan', 'Peru', 'Philippines', 'Russia', 'Thailand',
+                             'Turkey', 'United States of America', 'Vietnam'])
+    month_codebook = {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'}
+    title_name = 'CPI Prediction ' + 'of ' + disaster + ' in ' + month_codebook[month] + ' ' + str(year)
+    df_preds = pd.DataFrame({'country_name': country_name, 'CPI': y_preds})
+
+    return df_preds, title_name
+
+
+
+    
---
+++ custom_code/ML_models/realtime_earthquake_prediction.py
@@ -0,0 +1,300 @@
+
+# ML Model_
+# Realtime Earthquake Prediction in Next 7 Days using xgboost model
+
+# Module Requirements
+# numpy
+# pandas
+# matplotlib.pyplot
+# geopandas
+# geoviews
+# xgboost == 0.80
+# sklearn
+
+import pandas as pd
+import xgboost as xgb
+import numpy as np
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import datetime as dt
+import geoviews as gv
+from sklearn.model_selection import train_test_split
+
+gv.extension("bokeh")
+
+def plot_geomap(df, title):
+    """
+    Func plot_geomap is designed to show scatter plots on maps.
+
+    Attributes:
+    df(pd.DataFrame): Dataset we need(including 'longitude' and 'latitude' columns)
+    title(str): scatter plots title we need
+    """
+    assert isinstance(df, pd.DataFrame)
+    assert isinstance(title, str)
+
+    # WorldMap Background
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    world = world[(world.pop_est>0) & (world.name!="Antarctica")]
+
+    with plt.style.context(("seaborn", "ggplot")):
+        world.plot(figsize=(15,10),
+                color="white",
+                edgecolor = "grey");
+    
+    plt.scatter(df['longitude'], df['latitude'], s=df['mag']*10, color="red", alpha=0.3)
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+    plt.title(title)
+
+def plot_interactive_scattermap(df, title_name, point_size):
+    """
+    Func plot_interactive_sacttermap is designed to plot earthquake locations worldwide scatter map.
+
+    Attributes:
+    df(pd.DataFrame): Dataset with 'longitude' and 'latitude' info
+    title_name(str): Plot title name
+    point_size: Scatter plot point size
+
+    Returns:
+    Worldwide Scatter Map
+    """
+    assert isinstance(df, pd.DataFrame)
+    assert isinstance(title_name, str)
+
+    # WorldMap Background
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    world = world[(world.pop_est>0) & (world.name!="Antarctica")]
+    world_map = gv.Polygons(world)
+
+    points_on_map = gv.Points(df,
+                              kdims=["longitude", "latitude"],
+                              vdims=["mag"]).opts(color="tomato",
+                                                  size=point_size,
+                                                  line_color="black",
+                                                  hover_color="lime",
+                                                  tools=["hover", ],
+                                                  title=title_name,
+                                                  height=400,
+                                                  width=600)
+
+    return world_map * points_on_map
+
+def prepare_earthquake_data(days_out_to_predict = 7):
+    """
+    Func prepare_earthquake_data is designed to get dataset about Historical Earthquakes with Aggregated Longitude And Latitude.
+
+    Attributes:
+    days_out_to_predict(int): Rolling Windows
+
+    Returns:
+    df(pd.DataFrame): Dataset after Feature Extraction
+    df_live(pd.DataFrame): Dataset used to predict in the next 7 days
+    """
+    # get latest data from USGS servers
+    df = pd.read_csv('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv')
+
+    # Feature Extraction
+    df = df.sort_values('time', ascending=True)
+    # truncate time from datetime
+    df['date'] = df['time'].str[0:10]
+
+    # only keep the columns needed
+    df = df[['date', 'latitude', 'longitude', 'depth', 'mag', 'place']]
+    df = df[df['mag']>0]
+    df = df[df['date']<=dt.date.today().strftime('%Y-%m-%d')]
+    temp_df = df['place'].str.split(', ', expand=True) 
+    df['place'] = temp_df[1]
+    df = df[['date', 'latitude', 'longitude', 'depth', 'mag', 'place']]
+
+    # calculate mean lat lon for simplified locations
+    df_coords = df[['place', 'latitude', 'longitude']]
+    df_coords = df_coords.groupby(['place'], as_index=False).mean()
+    df_coords = df_coords[['place', 'latitude', 'longitude']]
+
+    # Merge the two dataframes of mean latitude and longitude locations calculated above with dataframe only considering ['date' , 'depth', 'mag', 'place']
+    df = df[['date', 'depth', 'mag', 'place']]
+    df = pd.merge(left=df, right=df_coords, how='inner', on=['place'])
+
+    # Feature Engineering and Data wrangling
+    # loop through each zone and apply MA
+    #eq_tmp = df.copy()
+    eq_data = []
+    df_live = []
+
+    for symbol in list(set(df['place'])):
+        temp_df = df[df['place'] == symbol].copy()
+        temp_df['depth_avg_22'] = temp_df['depth'].rolling(window=22,center=False).mean() 
+        temp_df['depth_avg_15'] = temp_df['depth'].rolling(window=15,center=False).mean()
+        temp_df['depth_avg_7'] = temp_df['depth'].rolling(window=7,center=False).mean()
+        temp_df['mag_avg_22'] = temp_df['mag'].rolling(window=22,center=False).mean() 
+        temp_df['mag_avg_15'] = temp_df['mag'].rolling(window=15,center=False).mean()
+        temp_df['mag_avg_7'] = temp_df['mag'].rolling(window=7,center=False).mean()
+        temp_df.loc[:, 'mag_outcome'] = temp_df.loc[:, 'mag_avg_7'].shift(days_out_to_predict * -1)
+
+        df_live.append(temp_df.tail(days_out_to_predict))
+
+        eq_data.append(temp_df)
+
+    # concat all location-based dataframes into master dataframe
+    df = pd.concat(eq_data)
+
+    # remove any NaN fields
+    df = df[np.isfinite(df['depth_avg_22'])]
+    df = df[np.isfinite(df['mag_avg_22'])]
+    df = df[np.isfinite(df['mag_outcome'])]
+    
+    # prepare outcome variable
+    # considered magnitude above 2.5 as dangerous hence prediction outcome as '1' elso '0'
+    df['mag_outcome'] = np.where(df['mag_outcome'] > 2.5, 1,0)
+
+    df = df[['date',
+             'mag',
+             'latitude',
+             'longitude',
+             'depth_avg_22',
+             'depth_avg_15',
+             'depth_avg_7',
+             'mag_avg_22', 
+             'mag_avg_15',
+             'mag_avg_7',
+             'mag_outcome']]
+
+    # keep only data where we can make predictions
+    df_live = pd.concat(df_live)
+    df_live = df_live[np.isfinite(df_live['mag_avg_22'])]
+    
+    return df, df_live
+
+def predict_earthquake_model(df, df_live, days_out_to_predict = 7, max_depth=3, eta=0.1):
+    """
+    Func predict_earthquake_model is designed to do earthquake prediction using xgboost.
+
+    Attributes:
+    df(pd.DataFrame): Dataset after Feature Extraction getting from Func: prepare_earthquake_data
+    df_live(pd.DataFrame): Dataset used to predict in the next 7 days getting from Func: prepare_earthquake_data
+    days_out_to_predict(int): Rolling Windows
+    max_depth(int): Hyperparameter for xgboost model
+    eta(float): Learning rate of the algorithm
+
+    Returns:
+    days_new(list): List of next 7 days 'yyyy-mm-dd'
+    df_output(pd.DataFrame): Most likely earthquake occurance location prediction in next 7 days.
+    """
+    assert isinstance(df, pd.DataFrame)
+    assert isinstance(df_live, pd.DataFrame)
+    assert isinstance(days_out_to_predict, int)
+    assert isinstance(max_depth, int)
+    assert isinstance(eta, float)
+    assert 0 < eta < 1
+
+    # Selection of features that are needed for prediction and hence consider only them rest are just ignored for prediction purpose.
+    features = [f for f in list(df) if f not in ['date', 'mag', 'mag_outcome', 'latitude', 'longitude']]
+
+    X_train, X_test, y_train, y_test = train_test_split(df[features], df['mag_outcome'], test_size=0.3, random_state=42)
+
+    dtrain = xgb.DMatrix(X_train[features], label=y_train)
+    dtest = xgb.DMatrix(X_test[features], label=y_test)
+
+    param = {
+            'objective': 'binary:logistic',
+            'booster': 'gbtree',
+            'eval_metric': 'auc',
+            'max_depth': max_depth,  # the maximum depth of each tree
+            'eta': eta,  # the training step for each iteration
+            'silent':1
+            }  # logging mode - quiet}  # the number of classes that exist in this datset
+
+    num_round = 1000  # the number of training iterations    
+    early_stopping_rounds=30
+    xgb_model = xgb.train(param, dtrain, num_round) 
+
+
+    # train on live data
+    dlive = xgb.DMatrix(df_live[features])  
+    preds = xgb_model.predict(dlive)
+
+    # add preds to live data
+    df_live_set = df_live[['date', 'place', 'latitude', 'longitude']].copy()
+    # add predictions back to dataset 
+    df_live_set.loc[:, 'mag'] = preds
+    #df_live_set = df_live_set.assign(preds=pd.Series(preds).values)
+
+    # aggregate down dups
+    df_live_set = df_live_set.groupby(['date', 'place'], as_index=False).mean()
+
+    # increment date to include DAYS_OUT_TO_PREDICT
+    df_live_set['date']= pd.to_datetime(df_live_set['date'],format='%Y-%m-%d')
+    #print(df_live_set.tail())
+    df_live_set['date'] = df_live_set['date'] + pd.to_timedelta(days_out_to_predict,unit='d')
+
+    #
+    days_new = list(set([d for d in df_live_set['date'].astype(str) if d > dt.datetime.today().strftime('%Y-%m-%d')]))
+    days_new.sort()
+    #days_new = days_new[0:7]
+    #print(days_new)
+    assert len(days_new) == days_out_to_predict
+    days_new_interval = (df_live_set['date'] >= days_new[0]) & (df_live_set['date'] <= days_new[-1])
+    df_output = df_live_set.loc[days_new_interval]
+    
+    return days_new, df_output
+
+def plot_roc(df, max_depth=3, eta=0.1):
+    """
+    Func plot_roc is designed to find the Receiver Operating Characteristic Plot
+
+    Attributes:
+    df(pd.DataFrame): Dataset after Feature Extraction getting from Func: prepare_earthquake_data
+    max_depth(int): Hyperparameter for xgboost model
+    eta(float): Learning rate of the algorithm
+    """
+    # Modules Import
+    from sklearn.metrics import roc_curve
+    from sklearn.metrics import auc
+    from sklearn.metrics import roc_auc_score
+    from sklearn.metrics import recall_score
+    from sklearn.metrics import confusion_matrix
+
+    assert isinstance(df, pd.DataFrame)
+    assert isinstance(max_depth, int)
+    assert isinstance(eta, float)
+    assert 0 < eta < 1
+
+    # Selection of features that are needed for prediction and hence consider only them rest are just ignored for prediction purpose.
+    features = [f for f in list(df) if f not in ['date', 'mag', 'mag_outcome', 'latitude', 'longitude']]
+    X_train, X_test, y_train, y_test = train_test_split(df[features], df['mag_outcome'], test_size=0.3, random_state=42)
+
+    dtrain = xgb.DMatrix(X_train[features], label=y_train)
+    dtest = xgb.DMatrix(X_test[features], label=y_test)
+
+    param = {
+            'objective': 'binary:logistic',
+            'booster': 'gbtree',
+            'eval_metric': 'auc',
+            'max_depth': max_depth,  # the maximum depth of each tree
+            'eta': eta,  # the training step for each iteration
+            'silent': 1}  # logging mode - quiet}  # the number of classes that exist in this datset
+    num_round = 5000  # the number of training iterations    
+    bst = xgb.train(param, dtrain, num_round)
+    preds = bst.predict(dtest)
+
+    print (roc_auc_score(y_test, preds))
+    fpr, tpr, _ = roc_curve(y_test, preds)
+    roc_auc = auc(fpr, tpr)
+    print('AUC:', np.round(roc_auc,4))
+
+    ypred_bst = np.array(bst.predict(dtest,ntree_limit=bst.best_iteration))
+    ypred_bst  = ypred_bst > 0.5
+    ypred_bst = ypred_bst.astype(int)  
+
+    plt.title('Receiver Operating Characteristic')
+    plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+    plt.legend(loc = 'lower right')
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.show()
+    print("Confusion Matrix: \n",confusion_matrix(y_test,ypred_bst))
+    print("\nRecall 'TP/TP+FN' = ", recall_score(y_test,ypred_bst))
