2b4f181cb9ba41428a99c5d2c936bf1b71f9a798
Cameron Lewis <ctl051@ucsd.edu>
2022-05-27 23:23:58 -0700

    Upgraded map visualization to use a selector for cases and date filter.
    Updated the major_disaster_distribution function so that we can come up
    with multiple cases we want to display on a holoviews map and select
    which one we want to display. It still needs fine tuning on the specific
    cases we want to cover and adding plot titles.

--- custom_code/major_disaster_analysis.py
+++ custom_code/major_disaster_analysis.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import geopandas    
 import geoviews as gv
 from cartopy import crs
+import panel as pn
+import datetime as dt
+import holoviews as hv
 gv.extension('bokeh')
 
 def death_vs_gdp(natural_disaster_df, gdp_df):
@@ -62,4 +65,68 @@ def major_disaster_distribution(natural_disaster_df):
         tools=['hover'], width=600,height=500, #projection=crs.Robinson()
     )
     return ax
+
+def map_distribution_cases(natural_disaster_df):
+    """
+    :natural_disaster_df The natural disaster dataframe
+    Plots a world map that displays the density of the countries that major disasters
+    took place. Can also display multiple types of dependent features. Expand on this
+    function to handle multiple cases to combine multiple visualizations into one.
+    """
+    assert isinstance(natural_disaster_df, pd.DataFrame)
+    def major_disaster_map(start_time,end_time,vis_type=None):
+        scale = 3/2+1/7
+        width=int(300*scale)
+        height=int(250*scale)
+        
+        restricted_df = natural_disaster_df[(start_time.year<=natural_disaster_df['Year']) &
+                                            (natural_disaster_df['Year']<=end_time.year)]
+        if vis_type == '# of Disasters':
+            
+            country_var = restricted_df.groupby('ISO')['Year'].count()
+            dep_var = 'Year'
+            dep_var_2 = 'Year_2'
+            feature_name = '# of Disasters'
+            
+        elif vis_type=='# of Deaths':
+            #calcualte the total number of deaths from all disasters within the restricted time period
+            country_var = restricted_df.groupby('ISO')['Total Deaths'].sum()
+            dep_var = 'Total Deaths'
+            dep_var_2 = 'TD_2'
+            feature_name = '# of Deaths'
+            pass
+        else:
+            return None
+        
+        # Import the geopandas map
+        world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+
+        # Avoid same column name problem
+        country_var = pd.DataFrame(country_var).rename(columns = {dep_var:dep_var_2})
+        country_var['inde'] = country_var.index
+        # Plot the count on the map
+        world = world.merge(country_var, left_on = 'iso_a3', right_on = 'inde', how = 'left').fillna(0)
+        ax = gv.Polygons(world, vdims =[(dep_var_2,feature_name), ('name','Country'),]).opts(
+            tools=['hover'], width=width,height=height, #projection=crs.Robinson()
+        )
+
+        bar = hv.Bars(country_var.sort_values(dep_var_2,ascending=False).iloc[:10],('inde','Country'),(dep_var_2,feature_name)).opts(
+            tools=['hover'], width=width,height=height, xrotation=70
+        )
+
+        return ax+bar
+        
+    min_date = dt.datetime(natural_disaster_df['Year'].min(),1,1)
+    max_date = dt.datetime(natural_disaster_df['Year'].max()+1,1,1)
+    dateslider= pn.widgets.DateRangeSlider(start=min_date,end=max_date,value=(min_date,max_date),name='Year Range')
+    selector = pn.widgets.Select(options=['# of Disasters','# of Deaths', 'Predicted Blank 2'], name='What to see?')
+    
+    streams = dict(start_time=dateslider.param.value_start,
+                   end_time=dateslider.param.value_end,
+                   vis_type=selector.param.value)
+    
+    
+    widget_dmap = hv.DynamicMap(major_disaster_map, streams=streams)
+    widget_dmap.opts(height=100)
+    return pn.Column(pn.Row(dateslider,selector),widget_dmap)
 
